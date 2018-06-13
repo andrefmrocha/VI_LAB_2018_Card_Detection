f3c9df225b96e932ed35cc258ff738d868c513a7
andrefmrocha <andrefmrocha@live.com.pt>
2018-06-13 18:00:46 +0100

    Base network made the smart way

--- CV_CNN.py
+++ CV_CNN.py
@@ -4,26 +4,30 @@ import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, MaxPooling2D, Activation, Flatten, Conv2D
+from keras.models import Model
 from keras import optimizers, losses
 from keras.regularizers import l2
 import matplotlib.pyplot as plt
 
 def create_base_network(input_shape, numb_conv32, numb_conv64):
     inputs = Input(shape=input_shape)
-    
-    if(numb_conv32 != 0):
-        x = Conv2(16, kernel_size={3,3}, activation='relu' )(inputs)
-        numb_conv32 = numb_conv32 - 1
-    
+    numb_conv32 /= 2
+    numb_conv64 /= 2
+    x = Conv2D(16, kernel_size={3,3}, activation='relu' )(inputs)
+    # if(numb_conv32 != 0):
+    #     numb_conv32 = numb_conv32 - 1
+
     for i in range(numb_conv32):
-        x= Conv2(32, kernel_size={3,3}, activation='relu')(x)
-        if(i%2 !=0):
-            MaxPooling2D(pool_size=(2,2)) 
-    
+        x= Conv2D(32, kernel_size={3,3}, activation='relu')(x)
+        x= Conv2D(32, kernel_size={3,3}, activation='relu')(x)
+        # if(i%2 !=0):
+        MaxPooling2D(pool_size=(2,2))
+
     for i in range(numb_conv64):
-        x= Conv2(64, kernel_size={3,3}, activation='relu')(x)
-        if(i%2 !=0):
-            MaxPooling2D(pool_size=(2,2)) 
-        
+        x= Conv2D(64, kernel_size={3,3}, activation='relu')(x)
+        x= Conv2D(64, kernel_size={3,3}, activation='relu')(x)
+        # if(i%2 ==0):
+        MaxPooling2D(pool_size=(2,2))
+
     return Model(inputs, x)
 
--- CV_main.py
+++ CV_main.py
@@ -1,5 +1,5 @@
 from CV_Dataset import *
-
+from CV_CNN import *
 from sklearn.model_selection import train_test_split
 x_Card, x_Camera = open_images()
 list_card, list_camera, list_labels = gen_data(x_Card, x_Camera)
