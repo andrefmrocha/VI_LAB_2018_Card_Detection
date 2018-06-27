99cdec1abaa5a029e9a93d68300d0aabd08dbb44
Joana Ferreira <joanaferreira0011@gmail.com>
2018-06-27 17:19:56 +0100

    Optimized parameters

--- CV_main.py
+++ CV_main.py
@@ -5,8 +5,8 @@ from keras.layers import concatenate
 from keras.utils import to_categorical
 
 x_Card, x_Camera = open_images()
-list_card, list_camera, list_labels = gen_data(x_Card, x_Camera, 3)
-# 60% - train, 20%, 20% - test, validation
+list_card, list_camera, list_labels = gen_data(x_Card, x_Camera)
+# 60% - train, 20%, 20% - test, validation))
 x_Train_Card, x_Test_Card, y_Train, y_Test= train_test_split(list_card, list_labels, test_size=0.4, random_state=1)
 x_Test_Card, x_Val_Card, y_Test, y_Val = train_test_split(x_Test_Card, y_Test, test_size=0.5, random_state=1)
 x_Train_Camera, x_Test_Camera= train_test_split(list_camera, test_size=0.4, random_state=1)
@@ -16,7 +16,7 @@ x_Train = {"card": x_Train_Card, "camera": x_Train_Camera}
 x_Test = {"card": x_Test_Card, "camera": x_Test_Camera}
 x_Val = {"card": x_Val_Card, "camera": x_Val_Camera}
 input_shape = [160,160,3]
-base_network, opt = create_base_network(input_shape,4,4)
+base_network, opt = create_base_network(input_shape,2,2)
 input_card = Input(shape=input_shape)
 input_camera = Input(shape=input_shape)
 
@@ -41,7 +41,7 @@ y_Test = to_categorical(y_Test, 2)
 cnn_model.compile(loss=xen_loss, optimizer= opt, metrics=['accuracy'])
 
 
-cnn_model.fit(x=[x_Train["card"], x_Train["camera"]], y=y_Train, epochs=50,
+cnn_model.fit(x=[x_Train["card"], x_Train["camera"]], y=y_Train, epochs=20,
               batch_size=32, validation_data=([x_Val["card"], x_Val["camera"]], y_Val))
 
 
