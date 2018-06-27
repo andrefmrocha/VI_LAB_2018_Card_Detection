7ae85d9b3a7ac91980df42aafb818f41818ab075
Joana Ferreira <joanaferreira0011@gmail.com>
2018-06-27 16:20:58 +0100

    fixed regularizers

--- CV_main.py
+++ CV_main.py
@@ -8,7 +8,7 @@ x_Card, x_Camera = open_images()
 list_card, list_camera, list_labels = gen_data(x_Card, x_Camera)
 # 60% - train, 20%, 20% - test, validation
 x_Train_Card, x_Test_Card, y_Train, y_Test= train_test_split(list_card, list_labels, test_size=0.4, random_state=1)
-x_Test_Card, x_Val_Card, y_Test, y_Val = train_test_split(x_Test_Card,y_Test, test_size=0.5, random_state=1)
+x_Test_Card, x_Val_Card, y_Test, y_Val = train_test_split(x_Test_Card, y_Test, test_size=0.5, random_state=1)
 x_Train_Camera, x_Test_Camera= train_test_split(list_camera, test_size=0.4, random_state=1)
 x_Test_Camera, x_Val_Camera= train_test_split(x_Test_Camera, test_size=0.5, random_state=1)
 
@@ -40,8 +40,8 @@ y_Test = to_categorical(y_Test, 2)
 cnn_model.compile(loss=xen_loss, optimizer= opt, metrics=['accuracy'])
 
 
-cnn_model.fit(x=[x_Train["card"],x_Train["camera"]], y = y_Train, epochs=20,
-              batch_size=32, validation_data=([x_Val["card"],x_Val["camera"]],y_Val))
+cnn_model.fit(x=[x_Train["card"], x_Train["camera"]], y=y_Train, epochs=20,
+              batch_size=32, validation_data=([x_Val["card"], x_Val["camera"]], y_Val))
 
 
 json_model = cnn_model.to_json()
